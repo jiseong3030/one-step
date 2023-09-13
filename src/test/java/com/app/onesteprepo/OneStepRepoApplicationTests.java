d7e13eaa257ffa001def554f6f43a1987cc5ea98
손성민 <alexzzanga7407@gmail.com>
2023-09-14 02:21:41 +0900

    init

---
+++ src/test/java/com/app/onesteprepo/OneStepRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.onesteprepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneStepRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

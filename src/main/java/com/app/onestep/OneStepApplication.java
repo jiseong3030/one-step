972d3300399237a07888ba26eade6d04b83a92e2
손성민 <alexzzanga7407@gmail.com>
2023-09-14 02:46:28 +0900

    fix

--- src/main/java/com/app/onestep/OneStepApplication.java
+++ src/main/java/com/app/onestep/OneStepApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OneStepRepoApplication {
+public class OneStepApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OneStepRepoApplication.class, args);
+		SpringApplication.run(OneStepApplication.class, args);
 	}
 
 }

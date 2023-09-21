c15636d544bb019a38cc5c5d2fe8edd4254c501e
BrianJoon <142293694+BrianJoon@users.noreply.github.com>
2023-09-21 11:23:32 +0900

    21일 임준영

--- src/main/resources/templates/Joon/customer-enquestions/customer-questions.js
+++ src/main/resources/templates/Joon/customer-enquestions/customer-questions.js
@@ -32,3 +32,29 @@ let modalok = document.getElementById("modal-ok");
 //         hobercate.style.display = "block";
 //     });
 // });
+
+$(function () {
+    // 드랍다운 쿼리
+    $(".head-dropdown1").click(function () {
+        $(".head-dropdown-menu").toggleClass("move");
+    });
+
+    $(".header-popover-content-font").click(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        let selectedMenuItem = $(this);
+        let selectedText = selectedMenuItem.text();
+
+        $(".orderby-status").text(selectedText);
+        $(".header-popover-content-font").removeClass("move");
+        selectedMenuItem.addClass("move");
+        $(".head-dropdown-menu").removeClass("move");
+    });
+
+    $(document).on("click", function (e) {
+        if (!$(e.target).closest(".head-dropdown1").length) {
+            $(".head-dropdown-menu").removeClass("move");
+        }
+    });
+
+});
